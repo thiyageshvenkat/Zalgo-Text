4ea16a4a056162f53f54ba2b6d66e8ba8eb8284f
thiyageshvenkat <60864803+thiyageshvenkat@users.noreply.github.com>
2024-07-11 18:08:22 -0700

    Create zalgo.cs

---
+++ zalgo.cs
@@ -0,0 +1,21 @@
+public class Zalgo {
+    public static string zalgo(string text, int intensity) {
+        Random rnd = new Random();
+        string result = "";
+        foreach (char ch in text) {
+            for (int i = 0; i < intensity; i++) {
+                // 768-800 marks the range of unicode where all the characters are diacritics.
+                result += (char) rnd.Next(768, 880); // Stacks another diacitic.         
+            }
+            result += ch; // At the end of all the diacritics for particular character, that character is added.
+        }
+        return result;
+    }
+}
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        string zalgoText = Zalgo.zalgo("Zalgo Text", 8);
+        Console.WriteLine(zalgoText);
+    }
+}
